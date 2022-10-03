95e6011c0d772256078e896b0396e7fd8a4b514b
Aaron <Harvey.AaronJ@gmail.com>
2022-10-03 12:17:32 -0400

    .

--- src/index.js
+++ src/index.js
@@ -3,16 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {
+			value: null
+		};
+	}
 	render () {
 		return (
-			<button className="square" onClick={() => console.log('click')}>
-				{this.props.value}
+			<button className='square' onClick={() => this.setState({ value: 'X' })}>
+				{this.state.value}
 			</button>
 		);
 	}
 }
 
+
+
+
+
 class Board extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {
+			squares: Array(9).fill(null)
+		};
+	}
 	renderSquare (i) {
 		return <Square value={i} />;
 	}
@@ -22,18 +38,18 @@ class Board extends React.Component {
 
 		return (
 			<div>
-				<div className="status">{status}</div>
-				<div className="board-row">
+				<div className='status'>{status}</div>
+				<div className='board-row'>
 					{this.renderSquare(0)}
 					{this.renderSquare(1)}
 					{this.renderSquare(2)}
 				</div>
-				<div className="board-row">
+				<div className='board-row'>
 					{this.renderSquare(3)}
 					{this.renderSquare(4)}
 					{this.renderSquare(5)}
 				</div>
-				<div className="board-row">
+				<div className='board-row'>
 					{this.renderSquare(6)}
 					{this.renderSquare(7)}
 					{this.renderSquare(8)}
@@ -43,14 +59,18 @@ class Board extends React.Component {
 	}
 }
 
+
+
+
+
 class Game extends React.Component {
 	render () {
 		return (
-			<div className="game">
-				<div className="game-board">
+			<div className='game'>
+				<div className='game-board'>
 					<Board />
 				</div>
-				<div className="game-info">
+				<div className='game-info'>
 					<div>{/* status*/}</div>
 					<ol>{/*TODO*/}</ol>
 				</div>
@@ -58,6 +78,10 @@ class Game extends React.Component {
 		);
 	}
 }
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Game />);
