c2d7771e6129d521abe8a002a1d80d82214d8c97
Aaron <Harvey.AaronJ@gmail.com>
2022-10-03 13:07:41 -0400

    Allows turn to change

--- src/index.js
+++ src/index.js
@@ -2,39 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
-	constructor (props) {
-		super(props);
-		this.state = {
-			value: null
-		};
-	}
-	render () {
-		return (
-			<button className='square' onClick={() => this.setState({ value: 'X' })}>
-				{this.state.value}
-			</button>
-		);
-	}
+function Square (props) {
+	return (
+		<button className='square' onClick={props.onClick}>
+			{props.value}
+		</button>
+	);
 }
-
-
-
-
 
 class Board extends React.Component {
 	constructor (props) {
 		super(props);
 		this.state = {
-			squares: Array(9).fill(null)
+			squares: Array(9).fill(null),
+			xIsNext: true
 		};
 	}
+
 	renderSquare (i) {
-		return <Square value={i} />;
+		return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+	}
+
+	handleClick (i) {
+		const squares = this.state.squares.slice();
+		squares[i] =
+			this.state.xIsNext ? 'X' :
+			'O';
+		this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
 	}
 
 	render () {
-		const status = 'Next player: X';
+		const status =
+			'Next player: ' +
+			(
+				this.state.xIsNext ? 'X' :
+				'O');
 
 		return (
 			<div>
@@ -59,10 +61,6 @@ class Board extends React.Component {
 	}
 }
 
-
-
-
-
 class Game extends React.Component {
 	render () {
 		return (
@@ -78,10 +76,6 @@ class Game extends React.Component {
 		);
 	}
 }
-
-
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Game />);
